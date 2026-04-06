7f876ab4992a807d00b6ce1c5e7c44dea8eb210d
loopbutinfinite <bestrada8800@gmail.com>
2026-04-06 13:48:42 -0700

    updated the page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-// app/page.tsx
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
-import { Card, CardContent } from "@/components/ui/card"
 import Image from "next/image";
 
 const specials = [
@@ -28,10 +19,23 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#f6f0f7] text-gray-800">
       {/* HERO */}
-      <section className="relative w-full h-[60vh] md:h-[70vh]">
+      <nav className="fixed top-0 w-full bg-[#f2ebfb]  px-4 z-50 shadow-sm">
+        <div className="container-fluid flex justify-between items-center">
+          <a href="#">
+            <img src="/assets/sakura-logo.png" alt="Sakura Logo" width={200} height={200} className="h-[120px] w-auto" />
+          </a>
+          <ul className="hidden lg:flex items-center space-x-5 text-[#9667e0] font-bold text-[16px] tracking-wider">
+            <a href="#homeSection" className=" hover:text-[#8a2be2] hover:underline transition-colors">HOME</a>
+            <a href="#dailySpecialsSection" className=" hover:text-[#8a2be2] hover:underline transition-colors">SPECIALS</a>
+            <a href="#productsSection" className=" hover:text-[#8a2be2] hover:underline transition-colors">PRODUCTS</a>
+            <a href="#aboutUsSection" className=" hover:text-[#8a2be2] hover:underline transition-colors">ABOUT</a>
+            <a href="#connectWithUsSection" className=" hover:text-[#8a2be2] hover:underline transition-colors">CONTACT</a>
+          </ul>
+        </div>
+      </nav>
+      <section id="homeSection" className="relative w-full h-[60vh] md:h-[70vh]">
         <div className="absolute inset-0 bg-black/30 z-10" />
         <div className="absolute inset-0">
-          {/* Replace with your own image */}
           <img src="/assets/Hero-Image.png" className="w-full h-full bg-gray-300" />
         </div>
 
@@ -44,46 +48,19 @@ export default function Home() {
               Stockton's favorite Asian grocery store.
             </p>
             <button className="bg-purple-500 hover:bg-purple-600 text-white px-5 py-2 rounded-lg transition">
-              Browse Products
+              <a href="#productsSection" className="font-bold">Browse Products</a>
             </button>
           </div>
         </div>
       </section>
 
       {/* DAILY SPECIALS */}
-      <section className="py-12 max-w-6xl h-137.5 mx-auto px-4">
-        <h2 className="text-2xl md:text-3xl font-semibold text-center mb-8">
-          Daily Specials
-        </h2>
-
-        <Carousel
-          opts={{
-            align: "start",
-          }}
-          className="w-full max-w-[12rem] sm:max-w-xs md:max-w-sm"
-        >
-          <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-3xl font-semibold">{index + 1}</span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </section>
+      {/* <Specials></Specials> */}
 
       {/* PRODUCTS SECTION */}
-      <section className="py-12 bg-white">
+      <section id="productsSection" className="py-12 bg-white">
         <div>
-          <h3 className="text-5xl text-center font-bold">Our Products</h3>
+          <h3 className="lg:text-5xl text-3xl underline text-center font-bold">Our Products</h3>
           <h5 className="text-3xl py-8 text-center"><span className="font-bold">Stockton's neighborhood hub</span> for authentic Asian flavors and essentials</h5>
         </div>
         <div className="grid grid-cols-2 px-30">
@@ -96,7 +73,7 @@ export default function Home() {
         </div>
       </section>
       {/* About Us Section */}
-      <section className="w-full px-4 md:px-6 lg:px-12 bg-pink-200">
+      <section id="aboutUsSection" className="w-full px-4 md:px-6 lg:px-12 bg-pink-200">
         <header className="text-center mt-6 lg:mt-10">
           <h1 className="lg:text-5xl text-3xl font-bold mb-3 underline pt-10">
             About Us
@@ -110,11 +87,11 @@ export default function Home() {
 
         {/* Our Story */}
         <div className="grid md:grid-cols-2 gap-6 mt-6 lg:mt-8 items-center">
-          <div>
-            <h2 className="text-4xl font-semibold underline mb-2">
+          <div className="lg:pb-20">
+            <h2 className="lg:text-4xl text-3xl font-semibold underline mb-2">
               Our Story
             </h2>
-            <p className="text-2xl">
+            <p className="text-3xl">
               Sakura Grocery Store was founded with a simple mission, to bring
               the richness of Japanese and other Asian cuisines to our Central Valley community. Inspired by the beauty
               and symbolism of the cherry blossom, sakura, our store represents renewal, tradition, and the joy of sharing.
@@ -134,7 +111,7 @@ export default function Home() {
 
         {/* What We Offer */}
         <div className="mt-8 lg:mt-10">
-          <h2 className="text-2xl font-semibold underline mb-4 text-center md:text-left">
+          <h2 className="text-3xl lg:text-4xl font-semibold underline mb-4 text-center md:text-left">
             What We Offer
           </h2>
 
@@ -148,10 +125,10 @@ export default function Home() {
                 height={100}
                 className="mx-auto mt-2"
               />
-              <p className="text-center mt-2">
+              <p className="text-center mt-2 lg:text-2xl">
                 <strong>Variety</strong>
               </p>
-              <ul className="mt-2 text-sm list-disc pl-4">
+              <ul className="mt-2 text-lg list-disc pl-4">
                 <li>
                   <strong>Wide Selection of Authentic Ingredients:</strong> Whether you’re cooking sushi,
                   ramen, or home-style Japanese dishes, you’ll find high-quality rice, noodles, sauces,
@@ -173,12 +150,12 @@ export default function Home() {
                 height={100}
                 className="mx-auto mt-2"
               />
-              <p className="text-center mt-2">
+              <p className="text-center mt-2 lg:text-2xl">
                 <strong>Ready-To-Eat</strong>
               </p>
-              <ul className="mt-2 text-sm list-disc pl-4">
+              <ul className="mt-2 text-lg list-disc pl-4">
                 <li>
-                  <strong>Ready-to-Eat & Grab-and-Go:</strong> We know life gets busy, so we stock tasty
+                  <strong>Ready-to-Eat:</strong> We know life gets busy, so we stock tasty
                   musubis, onigiri, bentos.
                 </li>
                 <li>
@@ -197,10 +174,10 @@ export default function Home() {
                 height={100}
                 className="mx-auto mt-2"
               />
-              <p className="text-center mt-2">
+              <p className="text-center mt-2 lg:text-2xl">
                 <strong>Snacks & Treats</strong>
               </p>
-              <ul className="mt-2 text-sm list-disc pl-4">
+              <ul className="mt-2 text-lg list-disc pl-4">
                 <li>
                   <strong>Snacks & Treats:</strong> From traditional Japanese wagashi and mochi to
                   modern-day snack trends, there’s something delightful at every turn.
@@ -215,12 +192,12 @@ export default function Home() {
         </div>
 
         {/* Vision */}
-        <div className="mt-10 space-y-6">
-          <div className="bg-gray-800 text-white rounded-xl p-6 text-center">
-            <h3 className="text-xl font-semibold underline mb-3">
+        <div className="mt-10 space-y-6 pb-10">
+          <div className="bg-gray-800 text-white rounded-xl p-6 text-center w-full">
+            <h3 className="lg:text-4xl text-3xl font-semibold underline mb-3">
               Our Vision
             </h3>
-            <p>
+            <p className="text-xl">
               We envision Sakura Grocery Store as more than just a grocery store - but a bridge between communities,
               a place where people of all backgrounds can come together through food. As we grow, we hope to continue
               expanding our offerings, sharing new flavors, and creating memorable experiences for everyone who walks
@@ -229,10 +206,10 @@ export default function Home() {
           </div>
 
           <div className="bg-gray-200 rounded-xl p-6 text-center">
-            <h3 className="text-xl font-semibold underline mb-3">
+            <h3 className="lg:text-4xl text-3xl font-semibold underline mb-3">
               Our Commitment to Stockton
             </h3>
-            <p>
+            <p className="text-xl">
               We believe in giving back to the community that has welcomed us. That means more than stocking your favorite
               foods. It means building genuine relationships with the community, whether it be through local events,
               cooking demonstrations, or partnerships with neighborhood organizations and communities, we’re dedicated to
@@ -244,68 +221,68 @@ export default function Home() {
       </section>
 
       {/* Connect With Us */}
-      <div className="container-fluid">
+      <div id="connectWithUsSection" className="container-fluid">
         <div className="row">
           <header
-            className="text-center justify-content-center my-lg-5 my-md-4 titleText my-4 text-5xl"><u>Connect With Us</u>
+            className="text-center justify-content-center my-lg-5 my-md-4 titleText text-5xl my-15"><u>Connect With Us</u>
           </header>
-          <div className="grid grid-cols-3">
-            <article className="mx-auto  justify-content-center connectWithUsCards">
-              <div className="bg-purple-200 rounded-2xl p-2">
+          <div className="grid lg:grid-cols-3 gap-10 lg:gap-0">
+            <article className="mx-auto h-full justify-content-center connectWithUsCards">
+              <div className="bg-purple-200 rounded-2xl p-2 w-72">
                 <h2 className="headerTitles mt-3 ms-3 text-3xl"><u>Contact Us</u></h2>
-                <div className=" mt-3 ms-3 mb-3">
-                  <img className="me-2" src="./assets/icon-phone.png" alt="phone icon" />
-                  <p className="lato-regular  align-items-end">(209)477-3939</p>
+                <div className=" mt-3 ms-3 mb-3 flex py-4">
+                  <img className="me-2 h-[50px]" src="./assets/icon-phone.png" alt="phone icon" />
+                  <p className="lato-regular my-auto align-items-end">(209)477-3939</p>
                 </div>
-                <div className=" mt-3 ms-3 mb-3">
-                  <img className="me-2" src="./assets/icon-mail.png" alt="email icon" />
-                  <p className="lato-regular  align-items-end">sakuragroceries@yahoo.com</p>
+                <div className=" mt-3 ms-3 mb-3 flex py-4">
+                  <img className="me-2 h-[50px]" src="./assets/icon-mail.png" alt="email icon" />
+                  <p className="lato-regular my-auto align-items-end">sakuragroceries@yahoo.com</p>
                 </div>
-                <div className=" mt-3 ms-3 mb-3">
-                  <img className="me-2" src="./assets/icon-map-pin.png" alt="map pin icon" />
-                  <p className="lato-regular  align-items-end">4343 Pacific Ave #A4<br />Stockton, CA 95207</p>
+                <div className=" mt-3 ms-3 mb-3 flex py-4">
+                  <img className="me-2 h-[50px]" src="./assets/icon-map-pin.png" alt="map pin icon" />
+                  <p className="lato-regular my-auto align-items-end">4343 Pacific Ave #A4<br />Stockton, CA 95207</p>
                 </div>
               </div>
             </article>
             <article className="mx-auto  justify-content-center connectWithUsCards">
-              <div className="bg-purple-200 rounded-2xl p-2">
+              <div className="bg-purple-200 rounded-2xl p-2 w-72">
                 <h2 className="text-3xl mt-3 ms-3"><u>Store Hours</u></h2>
-                <div className="d-flex mt-3 ms-3 mb-3">
-                  <img className="me-2" src="./assets/icon-clock.png" alt="clock icon" />
+                <div className="d-flex mt-3 ms-3 mb-3 flex py-4">
+                  <img className="me-2 h-[50px]" src="./assets/icon-clock.png" alt="clock icon" />
                   <p className="lato-regular ">Monday - Friday<br />10:00 AM - 7:00 PM</p>
                 </div>
-                <div className="d-flex mt-3 ms-3 mb-3">
-                  <img className="me-2" src="./assets/icon-clock.png" alt="clock icon" />
+                <div className="d-flex mt-3 ms-3 mb-3 flex py-4">
+                  <img className="me-2 h-[50px]" src="./assets/icon-clock.png" alt="clock icon" />
                   <p className="lato-regular">Saturday<br />9:00 AM - 6:00 PM</p>
                 </div>
-                <div className="d-flex mt-3 ms-3 mb-3">
-                  <img className="me-2" src="./assets/icon-clock2.png" alt="clock icon" />
+                <div className="d-flex mt-3 ms-3 mb-3 flex py-4">
+                  <img className="me-2 h-[50px]" src="./assets/icon-clock2.png" alt="clock icon" />
                   <p className="lato-regular">Sunday<br />CLOSED</p>
                 </div>
               </div>
             </article>
             <article className="mx-auto  justify-content-center connectWithUsCards">
-              <div className="bg-purple-200 rounded-2xl p-2">
+              <div className="bg-purple-200 rounded-2xl p-2 w-72">
                 <h2 className="text-3xl mt-3 ms-3"><u>Social Media</u></h2>
-                <div className="d-flex mt-3 ms-3 mb-3">
-                  <img className="me-2" src="./assets/icon-facebook.png" alt="facebook icon" />
-                  <p className="d-flex align-items-end lato-regular">Like us on &nbsp; <span
-                    className="socialMediaCardTextColor"><a target="_blank" className="socialMediaCardTextColor"
-                      href="https://www.facebook.com/pages/SAKURA-Japanese-groceries-and-gifts/160632486211">Facebook</a></span>
+                <div className="d-flex mt-3 mb-3 flex p-4">
+                  <img className="me-2 h-[50px]" src="./assets/icon-facebook.png" alt="facebook icon" />
+                  <p className="d-flex align-items-end lato-regular">Like us on <span
+                  ><a target="_blank"
+                    href="https://www.facebook.com/pages/SAKURA-Japanese-groceries-and-gifts/160632486211" className="text-purple-600 hover:text-purple-800 hover:underline">Facebook</a></span>
                   </p>
                 </div>
-                <div className="d-flex mt-3 ms-3 mb-3">
+                <div className="d-flex mt-3 ms-3 mb-3 flex py-4">
                   <img className="me-2" src="./assets/icon-instagram.png" alt="instagram icon" />
-                  <p className="d-flex align-items-end lato-regular">Follow us on &nbsp; <span
-                    className="socialMediaCardTextColor"><a target="_blank" className="socialMediaCardTextColor"
-                      href="https://www.instagram.com/sakuragroceries_stockton/?hl=en">Instagram</a></span>
+                  <p className="d-flex align-items-end lato-regular">Follow us on <span
+                  ><a target="_blank"
+                    href="https://www.instagram.com/sakuragroceries_stockton/?hl=en" className="text-purple-600 hover:text-purple-800 hover:underline">Instagram</a></span>
                   </p>
                 </div>
-                <div className="d-flex mt-3 ms-3 mb-3">
+                <div className="d-flex mt-3 ms-3 mb-3 flex py-4">
                   <img className="me-2" src="./assets/icon-yelp.png" alt="yelp icon" />
-                  <p className="d-flex align-items-end lato-regular">Find us on &nbsp; <span
-                    className="socialMediaCardTextColor"><a target="_blank" className="socialMediaCardTextColor"
-                      href="https://www.yelp.com/biz/sakura-stockton">Yelp</a></span></p>
+                  <p className="d-flex align-items-end lato-regular">Find us on <span
+                  ><a target="_blank" className="text-purple-600 hover:text-purple-800 hover:underline"
+                    href="https://www.yelp.com/biz/sakura-stockton">Yelp</a></span></p>
                 </div>
               </div>
             </article>
@@ -314,11 +291,15 @@ export default function Home() {
         <article className="row">
           <div className="col-12 d-flex justify-content-center connectWithUsCards visitUsCardResizing mx-auto">
             <div className="card cardBGColor visitUsCardResizing" />
-            <h2 className="text-center text-5xl justify-content-center my-3 contactUsCardsTitleText"><u>Visit Us</u>
+            <h2 className="text-center text-5xl justify-content-center mt-15 mb-10 contactUsCardsTitleText"><u>Visit Us</u>
             </h2>
-            <img src="./assets/Sakura-Google-Map-Location.png"
-              className="mx-auto pt-10 googleMapPinIMG ms-lg-3 ms-md-2 mb-3 mapPinIMG"
-              alt="Sakura Google Map Location Pin" />
+            <div className="w-full mx-auto shadow-2xl h-[400px] px-30">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6289.148040717251!2d-121.31411238796083!3d37.98706939978463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80900dabc8484c23%3A0x8662498d9fd2565f!2sSakura%20-%20Japanese%20Groceries%20%26%20Gifts!5e0!3m2!1sen!2sus!4v1775508299953!5m2!1sen!2sus"
+                className="w-full h-full border-0"
+                loading="lazy"
+              />
+            </div>
           </div>
         </article>
       </div>
--- components/Specials/page.tsx
+++ components/Specials/page.tsx
@@ -1,23 +1,30 @@
-const Hero = () => {
+"use client"
+
+import { Carousel } from "flowbite-react";
+
+
+
+
+
+const Specials = () => {
   return (
-    <section className="bg-[#1f1f1f] text-white px-6 md:px-12 py-16">
-      <h2 className="text-3xl md:text-4xl font-semibold mb-6">
-        Daily Specials
-      </h2>
-
-      <div className="bg-[#2a2a2a] p-6 rounded-xl shadow-lg">
-        <h3 className="text-xl mb-4">
-          Today's Special : <span className="font-bold">Beef Bowl</span>
-        </h3>
-
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 text-sm">
-          <div>Onigiri Rice Balls</div>
-          <div>Sushi Rolls</div>
-          <div>Inari Plate</div>
+    <>
+      <section id="dailySpecialsSection" className="py-12 max-w-6xl mx-auto px-4">
+        <h2 className="text-3xl md:text-5xl underline font-semibold text-center mb-8">
+          Daily Specials
+        </h2>
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+          <Carousel slideInterval={1000}>
+            <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+            <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+            <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+            <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+            <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+          </Carousel>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
-export default Hero;
+export default Specials;
